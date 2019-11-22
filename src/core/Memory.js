b6e80947af0f6973b52acd68aa06c2ecfa1a6b75
Arthur G <arthur@little.town>
2019-11-22 13:00:39 +0000

    Cleanup for memory.js

--- src/core/Memory.js
+++ src/core/Memory.js
@@ -8,14 +8,13 @@ class Memory {
   constructor(settings) {
     Object.assign(this, settings);
     this.mongo = mongo;
-    this.relationChecklist = [];
     this.routes = [];
   }
 
   async setupModels() {
     try {
       for (const name of Object.keys(models)) {
-        const { schema, methods } = models[name];
+        const { router, schema, methods } = models[name];
         const modelSchema = new Schema(schema, { timestamps: true });
 
         if (methods) {
@@ -23,19 +22,9 @@ class Memory {
         }
 
         this.mongo.model(name, modelSchema);
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
 
-  async setupRoutes() {
-    try {
-      for (const name of Object.keys(models)) {
-        const { router, schema, methods } = models[name];
-        const model = this.mongo.models[name];
         if (router) {
-          const generator = new APIDescription(router, name, model);
+          const generator = new APIDescription(router, name, this.mongo.models[name]);
           this.routes = this.routes.concat(await generator.create());
         }
       }
@@ -44,19 +33,10 @@ class Memory {
     }
   }
 
-  async load() {
-    try {
-      await this.setupModels();
-      await this.setupRoutes();
-    } catch (error) {
-      throw error; 
-    }
-  }
-
   async init() {
     try {
       await this.mongo.connect(this.host, this.config);
-      await this.load();
+      await this.setupModels();
     } catch (error) {
       throw error;
     }
